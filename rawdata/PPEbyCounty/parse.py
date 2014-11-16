02dde5ab07540028f12eb9bbaded182eddf4ba45
Michael Eden <themichaeleden@gmail.com>
2014-11-16 00:40:05 -0500

    Better parsing...

---
+++ rawdata/PPEbyCounty/parse.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+files = ['clean2004.txt', 'clean2005.txt', 'clean2006.txt', 'clean2007.txt', 'clean2008.txt', 'clean2009.txt', 'clean2010.txt', 'clean2011.txt', 'clean2012.txt', 'clean2013.txt']
+
+import csv
+import json
+
+# def leadZeros(data, num):
+#    return int(((len(data) - num) * "0") + data)
+
+def parseRow(row):
+    datum = {}
+    datum['leaRank'] = int(row[0])
+    datum['StatePPE'] = 
+    datum['StateRank'] = 
+    datum['FederalPPE'] = 
+    datum['FederalRank'] = 
+    datum['LocalPPE'] = 
+    datum['LocalRank'] = 
+    datum['TotalPPE'] = 
+    datum['TotalRank'] = 
